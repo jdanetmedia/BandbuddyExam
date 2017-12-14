74600bd3dfbb17a4a7dc6814f092754d6c10e31f
duplo1978 <duplo1978@live.dk>
2017-12-14 09:47:15 +0100

    Minor design and broken links

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -17,8 +17,8 @@ export class TabsPage {
 
     this.tab1root = 'FeedPage';
     this.tab2root = 'EditProfilePage';
-    this.tab3root = 'ProfilePage';
-    this.tab4root = 'ProfilePage';
+    this.tab3root = 'ArchivePage';
+    this.tab4root = 'NotificationsPage';
 
   }
 
