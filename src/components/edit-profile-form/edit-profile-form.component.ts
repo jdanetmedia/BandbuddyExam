ffd629250e4c9f8384db9cbcf03a3a6168aca8da
Jesper Dalsgaard <post@jdanet.dk>
2017-12-15 09:14:27 +0100

    user.uid tilføjet til filnavn i profilbillede

--- src/components/edit-profile-form/edit-profile-form.component.ts
+++ src/components/edit-profile-form/edit-profile-form.component.ts
@@ -60,7 +60,7 @@ export class EditProfileFormComponent implements OnDestroy {
 
       const image = `data:image/jpeg;base64,${result}`;
 
-      const pictures = storage().ref('pictures/profilbillede');
+      const pictures = storage().ref(`profilbilleder/profile-${this.authenticatedUser.uid}`);
       pictures.putString(image, 'data_url');
     }
     catch(e) {
