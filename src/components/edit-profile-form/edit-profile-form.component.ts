ebef3454fb5a53c35a3968c216fe348d208d1c83
Jesper Dalsgaard <post@jdanet.dk>
2017-12-15 11:07:16 +0100

    størrelse på billede fjernet

--- src/components/edit-profile-form/edit-profile-form.component.ts
+++ src/components/edit-profile-form/edit-profile-form.component.ts
@@ -49,8 +49,6 @@ export class EditProfileFormComponent implements OnDestroy {
       // Camera options
       const options: CameraOptions = {
         quality: 50,
-        targetWidth: 600,
-        targetHeight: 600,
         destinationType: this.camera.DestinationType.DATA_URL,
         encodingType: this.camera.EncodingType.JPEG,
         mediaType: this.camera.MediaType.PICTURE,
