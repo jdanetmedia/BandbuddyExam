895b13781d77c2908f8aad1fe861f557f64f2f4a
Jesper Dalsgaard <post@jdanet.dk>
2017-12-15 09:42:37 +0100

    Endnu et forsøg

--- src/components/edit-profile-form/edit-profile-form.component.ts
+++ src/components/edit-profile-form/edit-profile-form.component.ts
@@ -60,7 +60,8 @@ export class EditProfileFormComponent implements OnDestroy {
 
       const image = `data:image/jpeg;base64,${result}`;
 
-      const pictures = storage().ref('pictures/profilbillede' + this.authenticatedUser.uid);
+      // const pictures = storage().ref('pictures/profilbillede');
+      const pictures = storage().ref(`/profilbilleder/profil-${this.authenticatedUser.uid}`);
       pictures.putString(image, 'data_url');
     }
     catch(e) {
